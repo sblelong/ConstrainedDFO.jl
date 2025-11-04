ad4e7a64b2262bcf37268c31edcf59e92101c9ed
Sacha Benarroch-Lelong <sblelong+github@zaclys.net>
2025-11-03 22:27:48 -0500

    Actually take the budget into account in rMADS

--- src/solvers/rmads.jl
+++ src/solvers/rmads.jl
@@ -1,12 +1,17 @@
 using ManifoldsBase
 using NOMAD
+using Printf
 
 export rmads
 
 function local_bb_wrapper(f, M::AbstractManifold, p, v)
+    global n_evals
     d = get_vector(M, p, v)
     Pd = retract(M, p, d, ProjectionRetraction())
-    return (true, true, [f(Pd)])
+    fd = f(Pd)
+    n_evals += 1
+    @printf("%5d | %10.6f\n", n_evals, fd)
+    return (true, true, [fd])
 end
 
 function compute_subproblem_bounds(M::AbstractManifold, p)
@@ -14,35 +19,23 @@ function compute_subproblem_bounds(M::AbstractManifold, p)
     return -10.0 .* ones(q), 10.0 .* ones(q)
 end
 
-_doc_rMADS_problem = """
-```math
-    $(_tex(min))_{p∈$(_tex(:Cal, "M"))} f(p)
-```
-"""
-
-@doc """
-    rmads(M, f, max_bb_eval, p0 = rand(M); kwargs...)
-
-    Perforn the Mesh Adaptive Direct Search algorithm to find a solution to the problem
-
-    $_doc_rMADS_problem
-
-    where derivatives of `f` are not available.
-"""
 function rmads(M::AbstractManifold, f, max_bb_eval::Int, p0 = rand(M); kwargs...)
     q = manifold_dimension(M)
     ℓ = 0
     # Instantiating a solver state, or anything that stores at least the stopping criterion.
     p = p0
 
-    while ℓ < 100
+    global n_evals
+    n_evals = 0
+
+    while n_evals < max_bb_eval
         # Construct the local blackbox within ℝ^q
         local_bb(v) = local_bb_wrapper(f, M, p, v)
 
         # Create the q-dimensional problem for NOMAD
         lb, ub = compute_subproblem_bounds(M, p)
         local_problem_budget = div(max_bb_eval, 10)
-        local_problem_options = NOMAD.NomadOptions(max_bb_eval = local_problem_budget)
+        local_problem_options = NOMAD.NomadOptions(max_bb_eval = local_problem_budget, display_degree = 0)
         local_problem = NomadProblem(q, 1, ["OBJ"], local_bb; lower_bound = lb, upper_bound = ub, options = local_problem_options)
 
         # Solve the subproblem
@@ -51,7 +44,7 @@ function rmads(M::AbstractManifold, f, max_bb_eval::Int, p0 = rand(M); kwargs...
         dℓ = get_vector(M, p, vℓ)
         new_p = retract(M, p, dℓ)
         fℓ = f(new_p)
-        println("f(p) = $(fℓ)")
+        # println("f(p) = $(fℓ), n_evals=$(n_evals)")
         η = norm(p .- new_p)
         p = new_p
 
