f52df67d09ed2cdcd6d55272bb0b7e57048e5547
Sacha Benarroch-Lelong <sblelong+github@zaclys.net>
2025-12-15 09:59:47 +0100

    Update stdout redirection tool

--- src/tools/redirect.jl
+++ src/tools/redirect.jl
@@ -3,7 +3,6 @@ export redirect_to_files
 function redirect_to_files(dofunc, outfile)
     return open(outfile, "w") do out
         redirect_stdout(out) do
-
             dofunc()
         end
     end
