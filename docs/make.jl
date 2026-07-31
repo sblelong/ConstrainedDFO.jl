445d8a0cca7f4b1e86bcae99409d4c7238e7ad3f
sblelong <sblelong+github@zaclys.net>
2026-07-31 16:58:51 +0200

    Fix deploydocs() call

--- docs/make.jl
+++ docs/make.jl
@@ -1,7 +1,5 @@
 using Documenter, ConstrainedDFO
 
 deploydocs(
-    repo = "github.com/sblelong/ConstrainedDFO.jl.git",
-    sitename="ConstrainedDFO.jl",
-    remotes = nothing
+    repo = "github.com/sblelong/ConstrainedDFO.jl.git"
 )
