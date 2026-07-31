c3f3e1bd81b4365f4718840cc2a24373db1299b2
sblelong <sblelong+github@zaclys.net>
2026-07-31 19:28:48 +0200

    Add makedocs again to docs/make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,5 +1,11 @@
 using Documenter, ConstrainedDFO
 
+makedocs(
+    sitename="ConstrainedDFO.jl",
+    remotes = nothing,
+    modules = [ConstrainedDFO]
+)
+
 deploydocs(
-    repo = "github.com/sblelong/ConstrainedDFO.jl.git"
+    repo = "github.com/sblelong/ConstrainedDFO.jl.git",
 )
