17d4d9a296cf44b642d8d1986bf7b7f8011ed7e8
sblelong <sblelong+github@zaclys.net>
2026-08-23 18:15:56 +0200

    Typo in a caps

--- src/solvers/tangent_solvers/TangentSolver.jl
+++ src/solvers/tangent_solvers/TangentSolver.jl
@@ -103,7 +103,7 @@ end
 Solve the subproblem
 
 ```math
-    \\begin{array}{r l L}
+    \\begin{array}{r l l}
         \\min\\limits_{v\\in T_p\\mathcal{M}} & f\\circ R_p(v)            \\\\
         \\mathrm{s.t.}                        & g\\circ R_p(v) \\leq 0
     \\end{array}
