21910ea81b142e42c8a7c288dcbfa01ea7dbe1bf
sblelong <sblelong+github@zaclys.net>
2026-09-10 13:05:11 +0200

    try/catch with whole NOMAD log

--- src/solvers/tangent_solvers/TangentSolver.jl
+++ src/solvers/tangent_solvers/TangentSolver.jl
@@ -52,21 +52,33 @@ function retract_eval_store!(
         εeqs::Float64 = 1.0e-8
     )
     d = get_vector(M, p, v, DefaultOrthonormalBasis())
-    Rpv = retract(M, p, d, R)
+    try
+        Rpv = retract(M, p, d, R)
+        fRpv = is_point_dispatcher(M, Rpv; tol_eqs = εeqs) ? get_cost(M, mco, Rpv) : FAILURE_MAX
+        if n_ineqs > 0
+            gRpv = g(Rpv)
+        else
+            gRpv = Float64[]
+        end
+        eval_data = BlackboxTangentData(d, Rpv, fRpv, gRpv)
 
-    fRpv = is_point_dispatcher(M, Rpv; tol_eqs = εeqs) ? get_cost(M, mco, Rpv) : FAILURE_MAX
+        _store_eval_data!(TS, eval_data)
 
-    if n_ineqs > 0
-        gRpv = g(Rpv)
-    else
-        gRpv = Float64[]
+        return eval_data
+    catch e
+        Rpv = p
+        fRpv = FAILURE_MAX
+        if n_ineqs > 0
+            gRpv = fill(FAILURE_MAX, n_ineqs)
+        else
+            gRpv = Float64[]
+        end
+        eval_data = BlackboxTangentData(d, Rpv, fRpv, gRpv)
+
+        _store_eval_data!(TS, eval_data)
+
+        return eval_data
     end
-
-    eval_data = BlackboxTangentData(d, Rpv, fRpv, gRpv)
-
-    _store_eval_data!(TS, eval_data)
-
-    return eval_data
 end
 
 """
