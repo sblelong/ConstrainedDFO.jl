992b478be3a316962eb746de8dbeed83c389fede
sblelong <sblelong+github@zaclys.net>
2026-09-12 22:01:47 +0200

    Fix nlp_to_bb utils to the API of NLPModels

--- benchmark/utils/nlp_models_utils.jl
+++ benchmark/utils/nlp_models_utils.jl
@@ -9,18 +9,27 @@ function nlp_to_bb(nlp::AbstractNLPModel)
 
     # Constraints
     n_cons = nlp.meta.ncon
+
     # Equalities
     idcs_eqs = nlp.meta.jfix
     n_eqs = length(idcs_eqs)
-    h(x) = cons(nlp, x)[idcs_eqs]
+    h(x) = cons(nlp, x)[idcs_eqs] .- nlp.meta.ucon[idcs_eqs]
 
     # Inequalities and bounds altogether
     idcs_ineqs = setdiff(1:n_cons, idcs_eqs)
     n_ineqs = length(idcs_ineqs)
-    lbounds(x) = [nlp.meta.lvar[idx] - x[idx] for idx in [nlp.meta.ilow ; nlp.meta.irng]]
-    ubounds(x) = [x[idx] - nlp.meta.uvar[idx] for idx in [nlp.meta.iupp ; nlp.meta.irng]]
-
-    g(x) = [-1.0 .* cons(nlp, x)[idcs_ineqs] ; lbounds(x) ; ubounds(x)] # Careful! NLPModels uses g(x)≥0.
+    function g(x)
+        cons_values = cons(nlp, x)
+        idcs_ineqs_l = [nlp.meta.jlow ; nlp.meta.jrng] # Indices of constraints of the form c≤g(x)
+        lineqs = nlp.meta.lcon[idcs_ineqs_l] .- cons_values[idcs_ineqs_l]
+        idcs_ineqs_u = [nlp.meta.jupp ; nlp.meta.jrng]
+        uineqs = cons_values[idcs_ineqs_u] .- nlp.meta.lcon[idcs_ineqs_u]
+        idcs_lbounds = [nlp.meta.ilow ; nlp.meta.irng]
+        lbounds = [nlp.meta.lvar[idx] - x[idx] for idx in idcs_lbounds]
+        idcs_ubounds = [nlp.meta.iupp ; nlp.meta.irng]
+        ubounds = [x[idx] - nlp.meta.uvar[idx] for idx in idcs_ubounds]
+        return [lineqs ; uineqs ; lbounds ; ubounds]
+    end
 
     problem = BlackboxProblem(dimension, n_eqs, n_ineqs, f, h, g)
 
