16ecbb21e69a857e64a5eb4a57e3aba300089ab3
sblelong <sblelong+github@zaclys.net>
2026-09-13 18:40:59 +0200

    Project p0 to M if p0 is infeasible

--- src/solvers/DFROSolver.jl
+++ src/solvers/DFROSolver.jl
@@ -65,7 +65,7 @@ function DFROSolver(
     end
 
     outer_counter = 0
-    p = p0
+    p = is_point(M, p0; tol_eqs = tol_eqs) ? p0 : project(M, p0)
     remaining_eval_budget = max_evals
     termination::Bool = false
     while !termination
