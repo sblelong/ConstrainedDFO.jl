ecf474d18e8f3306716897b785cc66795cab24a1
sblelong <sblelong+github@zaclys.net>
2026-09-18 16:05:59 +0200

    Fix spheres defining functions

--- src/types/ScaledSphere.jl
+++ src/types/ScaledSphere.jl
@@ -13,8 +13,8 @@ get_embedding(M::ScaledSphere) = Euclidean(representation_size(M)...)
 get_radius(M::ScaledSphere) = M.radius
 get_radius(M::Manifolds.Sphere) = 1.0
 
-eval_defining_function(M::Manifolds.Sphere, p) = norm(p)^2 - 1.0
-eval_defining_function(M::ScaledSphere, p) = norm(p)^2 - get_radius(M)
+eval_defining_function(M::Manifolds.Sphere, p) = [norm(p)^2 - 1.0]
+eval_defining_function(M::ScaledSphere, p) = [norm(p)^2 - get_radius(M)]
 
 function check_point(M::ScaledSphere, p; kwargs...)
     if !isapprox(norm(p), M.radius; kwargs...)
