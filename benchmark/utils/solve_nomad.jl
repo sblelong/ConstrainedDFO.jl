cfb78ff226ef89cd00187bba24d89ffc30429651
sblelong <sblelong+github@zaclys.net>
2026-09-24 04:33:40 -0400

    Utils script to solve problems with linear converters in NOMAD

--- benchmark/utils/solve_nomad.jl
+++ benchmark/utils/solve_nomad.jl
@@ -25,3 +25,23 @@ function solve_nomad(BI::BlackboxInstance; barrier::Symbol = :PB, max_evals::Int
     pb = NomadProblem(dimension, 1 + n_constraints, output_types, blackbox; options = options)
     return result = solve(pb, x0)
 end
+
+function solve_nomad_converter(BI::BlackboxInstance, A::Matrix{Float64}, b::Vector{Float64}; converter = :SVD, barrier = :PB, max_evals = 1000 * (get_dimension(BI) + 1), tol_eqs::Float64 = 1.0e-8)
+    dimension = get_dimension(BI)
+    n_ineqs = get_n_ineqs(BI)
+    output_types = [["OBJ"] ; [String(barrier) for _ in 1:n_ineqs]]
+    x0 = ConstrainedDFO.get_x0(BI)
+    lb = get_lower_bounds(BI)
+    ub = get_upper_bounds(BI)
+
+    function blackbox(x)
+        h = eval_eqs(BI, x)
+        f = isapprox(h, 0.0; atol = tol_eqs) ? eval_objective(BI, x) : ConstrainedDFO.FAILURE_MAX
+        g = eval_ineqs(BI, x)
+        return (true, true, [[f] ; g])
+    end
+
+    options = NOMAD.NomadOptions(max_bb_eval = max_evals, display_stats = ["BBE", "BBO"], display_all_eval = true, linear_converter = String(converter), linear_constraints_atol = tol_eqs)
+    pb = NomadProblem(dimension, 1 + n_ineqs, output_types, blackbox; A = A, b = b, lower_bound = lb, upper_bound = ub, options = options)
+    return solve(pb, x0)
+end
