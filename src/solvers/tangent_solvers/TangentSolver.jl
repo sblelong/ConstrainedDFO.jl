08a3082dfe94415c32253211027b43b5337942cc
sblelong <sblelong+github@zaclys.net>
2026-09-25 06:13:16 -0400

    Add tolerances as kwargs to the solve method for tangent solvers

--- src/solvers/tangent_solvers/TangentSolver.jl
+++ src/solvers/tangent_solvers/TangentSolver.jl
@@ -114,7 +114,7 @@ function blackbox_wrapper_store!(
 end
 
 """
-    solve!(TS::AbstractTangentSolver, mco::AbstractManifoldCostObjective, M::AbstractManifold, p, R::AbstractRetractionMethod, ρ::AbstractInvertibilityBound; g)
+    solve!(TS::AbstractTangentSolver, mco::AbstractManifoldCostObjective, M::AbstractManifold, p, R::AbstractRetractionMethod, invertibility_radius::Float64, n_ineqs::Int; g, max_evals, εeqs, εineqs)
 
 Solve the subproblem
 
@@ -137,7 +137,7 @@ function solve!(
         R::AbstractRetractionMethod,
         invertibility_radius::Float64,
         n_ineqs::Int;
-        g, max_evals::Int, εeqs::Float64 = 1.0e-8
+        g, max_evals::Int, εeqs::Float64 = 1.0e-8, εineqs::Float64 = 1.0e-8
     )
 end
 
